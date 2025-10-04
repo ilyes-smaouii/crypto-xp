fbe8d02b0919fd539de558901e44154f2e94f7c6
Ilyes <ilyes.smaoui94@gmail.com>
2025-10-04 03:28:01 +0200

    Minor change

--- src/crypto_helpers.cpp
+++ src/crypto_helpers.cpp
@@ -1,4 +1,5 @@
 #include "crypto_helpers.hpp"
+#include <string>
 
 std::size_t get_string_size_in_memory(const std::string &some_str) {
   constexpr std::size_t CHAR_SIZE = sizeof(std::string::traits_type::char_type);
